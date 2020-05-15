bb6ec643135bcfe99f9f245c5aca85f2e74bd4db
Tamil Selvan K <tmlslvn6@hotmail.com>
2020-05-15 17:20:50 +0530

    V1.0

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@ionic/angular';
+import { Platform, ToastController } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Plugins } from '@capacitor/core';
+const { App } = Plugins;
 
 @Component({
   selector: 'app-root',
@@ -13,7 +15,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private toastCtrl: ToastController
   ) {
     this.initializeApp();
   }
@@ -22,6 +25,35 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.registerBackButtonListener();
     });
   }
+
+  registerBackButtonListener() {
+    const rootScope = {
+      backButtonPressedOnceToExit: false
+    };
+    this.platform.backButton.subscribeWithPriority(10, async () => {
+      // if (!this.routerOutlet.canGoBack()) {
+      // App.exitApp();
+      // }
+
+      if (rootScope.backButtonPressedOnceToExit) {
+        App.exitApp();
+      } else {
+        rootScope.backButtonPressedOnceToExit = true;
+        (await this.toastCtrl.create({
+          message: 'Press back button again to exit',
+          duration: 2000
+        })).present();
+        setTimeout(() => {
+          rootScope.backButtonPressedOnceToExit = false;
+        }, 2000);
+      }
+
+    });
+  }
+
+
+
 }
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -6,16 +9,64 @@ import { Component } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page {
-  txtloanamt = 0;
-  txtint = 0;
-  txtmonth = 0;
-  txtfees = 0;
-  txttax = 0;
-  constructor() { }
 
+  indata = {
+    txtloanamt: '',
+    txtint: '',
+    txtmonth: '',
+    txtfees: '',
+    txttax: '',
+  };
+  iscalc: any = false;
+  rate: any;
+  principal: any;
+  nterms: any;
+  ratepow: any;
+  resultEMI: any;
+  resultLoan: any;
+  resultExtra: any;
+  resultFees: any;
+  resultRepay: any;
+  constructor(public toastController: ToastController) {
+
+  }
+
+  async presentToast(msg) {
+    const toast = await this.toastController.create({
+      message: msg,
+      position: 'middle',
+      duration: 2000
+    });
+    toast.present();
+  }
 
   logForm() {
-    console.log('Hi Got it');
+    console.log(this.indata);
+
+    if (Number(this.indata.txtint) <= 0) {
+      this.presentToast('Interest must be greater than zero');
+      return false;
+    } else if (Number(this.indata.txtloanamt) <= 0) {
+      this.presentToast('Loan Amount must be greater than zero');
+      return false;
+    } else if (Number(this.indata.txtmonth) <= 0) {
+      this.presentToast('Loan Tenure must be greater than zero');
+      return false;
+    }
+
+    this.rate = Number((Number(this.indata.txtint) / 12 / 100).toFixed(5));
+    console.log(this.rate);
+    this.principal = Number(this.indata.txtloanamt);
+    this.nterms = Number(this.indata.txtmonth);
+    this.ratepow = Math.pow((1 + this.rate), this.nterms);
+    console.log(this.ratepow);
+    this.resultEMI = Number((this.principal * this.rate * (this.ratepow / (this.ratepow - 1))).toFixed(2));
+
+    this.resultLoan = this.principal.toFixed(2);
+    this.resultExtra = Number(((this.resultEMI * this.nterms) - this.principal).toFixed(2));
+    this.resultFees = Number((((this.resultExtra + Number(this.indata.txtfees)) / 100) * Number(this.indata.txttax)).toFixed(2));
+    this.resultRepay = (this.principal + this.resultExtra + this.resultFees + Number(this.indata.txtfees)).toFixed(2);
+    this.iscalc = true;
   }
 
 
